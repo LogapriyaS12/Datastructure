88fddd5f5c8006065b6ff9347f208e8422976086
LogapriyaS12 <logapriya9363@gmail.com>
2025-03-22 14:37:01 +0530

    SumOfElements.java

---
+++ SumOfElements.java
@@ -0,0 +1,33 @@
+import java.util.*;
+class node{
+    int data;
+    node nxt;
+}
+class SumOfElements{
+    public static void insert(node head){
+        node ob = new node();
+        Scanner sc = new Scanner(System .in);
+        ob.data = sc.nextInt();
+        ob.nxt =head.nxt;
+        head.nxt =ob;
+    }
+    public static void display(node head){
+        node t = head.nxt;
+        while(t!=null){
+            System.out.println(t.data);
+            t=t.nxt;
+        }
+    }
+    public static void main(String[] main){
+        node head =new node();
+        head.nxt = null;
+        insert(head);
+        insert(head);
+        insert(head);
+        insert(head);
+        display(head);
+
+    }
+
+}
+    
